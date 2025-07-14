3b019774948cbf53591e658e4a05c34b3352c3e6
Prakash Gujarati <prakash@vsc.edu.in>
2025-07-14 08:59:22 +0530

    i have add program for sum

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const name = "Prakash Gujarati"
 // console.log("Hello "+name)
 console.log(`Hello ${name}`)
 
+console j = 5
+console sum = 1;
+for(i=1;i<j;i++)
+{
+    sum += j
+}
+console.log(sum)
+    
 //console.log("Enter Number N:")
 const n = 5
-// console.log(n%2)
-
 console.log("\n3. Program Even ODD")
 if(n%2)
 {
