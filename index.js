184fbe079b3fba5cdb5d3013694537ab9f384e0d
PrakashGujarati <prakash@vsc.edu.in>
2025-07-14 08:56:57 +0530

    init with basic program

---
+++ index.js
@@ -0,0 +1,17 @@
+const name = "Prakash Gujarati"
+// console.log("Hello "+name)
+console.log(`Hello ${name}`)
+
+//console.log("Enter Number N:")
+const n = 5
+// console.log(n%2)
+
+console.log("\n3. Program Even ODD")
+if(n%2)
+{
+    console.log("Odd Number")
+}else
+{
+    console.log("Even Number")
+}
+
